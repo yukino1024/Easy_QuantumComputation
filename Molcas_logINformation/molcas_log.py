70bc5defc2302f0838164525d13cf393cf0c5a91
yukino1024 <yukino1024@qq.com>
2025-05-25 17:17:25 +0800

    Add files via upload

---
+++ Molcas_logINformation/molcas_log.py
@@ -0,0 +1,21 @@
+import os
+from Myclass.Readlog import read
+from Myclass.Ras_energy import ras_ene
+from Myclass.Pt2_energy import pt2_ene
+from Myclass.Ci_occupy import ci_coo
+from Myclass.Osc_strength import osc_str
+from Myclass.Write_excel import wri_excel
+
+def do_write(log_adress):
+    excel_name = os.path.splitext(os.path.basename(log_adress))[0] + '.xlsx'
+    excel_adress = os.path.join(os.path.dirname(log_adress), excel_name)
+    s = read(log_adress)
+    r = ras_ene(s.ras_energy)
+    p = pt2_ene(s.pt2_energy)
+    c = ci_coo(s.occ)
+    o = osc_str(s.osc)
+    w = wri_excel()
+    for i in range(1, r.get_num + 1):
+        w.add_data(i, r.get(i), p.get(i), o.get(i), c.get(i))
+        w.wri_data()
+        w.save_excel(excel_adress)
